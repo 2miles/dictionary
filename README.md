bcf3e5b27f309683bf95ea4c4e4a8182fa5a48ad
miles <miles4599@gmail.com>
2022-04-07 22:17:24 -0700

    added synonyms to output

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Command line dictionary
-This is a simple dictionary program that takes a word as input and scrapes vocabulary.com for definitions. It then outputs the results as a numbered list.
+This is a simple dictionary program that scrapes vocabulary.com for definitions and synomyms of a word. Given a word, it outputs all the definitions as a numbered list followed by a list of synomyms.
 
 ## How to use
-Run the define.py script with the word you want to define given as an argument. The script can be run without any arguments to list the definitions of a random word.
+Run the define.py script with the word you want to define given as an argument. If the word contains a " " use a "-" instead. The script can be run without any arguments to list the definitions of a random word.
 
 ## Requirements
 This script requires: 
--- define.py
+++ define.py
@@ -2,21 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-# get word from command line
-wordList = sys.argv[1:]
-# 'word' is considered as one term seperated by spaces
-word = " ".join(wordList)
+word = ""
+if len(sys.argv) != 1:
+    word = sys.argv[1].replace("-", " ")
+
 # get the html at vocabulary.com for 'word'
 if word == "":
     word = "randomword"
 source = requests.get(f"https://www.vocabulary.com/dictionary/{word}").text
 soup = BeautifulSoup(source, "lxml")
+
 if word == "randomword":
     word = soup.find("span", class_="word").text
 
 definitions_exist = False
 
-print(f"\nDefinition of {word}:\n")
+print(f"\n-------- Definitions of {word}:\n")
 for count, dfn in enumerate(soup.find_all("div", class_="definition")):
     definitions_exist = True
     if dfn != None:
@@ -29,3 +30,12 @@ for count, dfn in enumerate(soup.find_all("div", class_="definition")):
             print(f"{count + 1}. ({typ})   {dfn}")
 if definitions_exist == False:
     print(f"No definitions for '{word}'")
+print()
+
+print(f"\n---------- Synonyms of {word}:\n")
+for count, dfn in enumerate(soup.find_all("a", class_="word")):
+    syn = dfn.text
+    print(f"{syn}, ", end="")
+    if (count + 1) % 7 == 0:
+        print()
+print("\n\n")
